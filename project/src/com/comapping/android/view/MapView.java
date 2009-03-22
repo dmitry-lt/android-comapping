28e549db3ce6f6e8edd17ea911cb8b6f55435a2a
Vldimir.Kulikov <Vldimir.Kulikov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-03-22 16:16:32 +0000

    We can open/close topics now

--- project/src/com/comapping/android/view/MapView.java
+++ project/src/com/comapping/android/view/MapView.java
@@ -1,5 +1,6 @@
 package com.comapping.android.view;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.comapping.android.model.Map;
@@ -10,10 +11,16 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MapView extends View {
 
+	public MapView(Context context, Map map) {
+		super(context);
+		this.map = map;
+	}	
+	
 	private static final int xShift = 30;
 	private static final int yShift = 30;
 	private static final int oSize = 10;
@@ -24,10 +31,19 @@ public class MapView extends View {
 	
 	private int x;
 	private int y;
+	private boolean toUpdate = true;
+	private ArrayList<EventPoint> es = new ArrayList<EventPoint>();
 	
-	public MapView(Context context, Map map) {
-		super(context);
-		this.map = map;
+	private class EventPoint
+	{
+		public int x, y, id;
+		
+		public EventPoint(int x, int y, int id)
+		{
+			this.x = x;
+			this.y = y;
+			this.id = id;
+		}
 	}
 	
 	private void drawTopic(Topic topic, Canvas c)
@@ -41,6 +57,8 @@ public class MapView extends View {
 		if (topic.getChildrenCount() > 0)
 		{
 			c.drawCircle(x, y, oSize, p);
+			if (toUpdate)
+				es.add(new EventPoint(x, y, id));
 			if (open.get(id) == null)
 				open.put(id, true);
 		}
@@ -76,14 +94,55 @@ public class MapView extends View {
 				c.drawLine(x, ty - iSize, x, ty + iSize, p);
 		}
 	}
-		
+
+	int count = 0;
+	long FPS = 0;
+	long startTime = System.currentTimeMillis();
+	boolean test = true;
+	
 	@Override
 	protected void onDraw(Canvas c)
 	{
-		c.drawARGB(255, 255, 255, 255);
+		if (test)
+			c.drawARGB(255, 255, 255, 255);
+		else
+			c.drawARGB(0, 255, 255, 255);
 		x = 15;
-		y = 15;
+		y = 25;
+		if (toUpdate)
+			es.clear();
 		drawTopic(map.getRoot(), c);
+		toUpdate = false;
+		
+		Paint p = new Paint();
+		p.setColor(Color.GREEN);
+		c.drawText("FPS: " + FPS, 10, 10, p);
+		if (System.currentTimeMillis() - startTime > 1000) {
+			FPS = (1000 * count) / (System.currentTimeMillis() - startTime);
+			startTime = System.currentTimeMillis();
+			count = 0;
+		}
+		count++;
+		invalidate();
+		
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+
+		if (event.getAction() == MotionEvent.ACTION_DOWN)
+		{
+			int x = (int)event.getX();
+			int y = (int)event.getY();
+			for (EventPoint e : es)
+				if (Math.hypot(x - e.x, y - e.y) < oSize)
+				{
+					open.put(e.id, !open.get(e.id));
+					toUpdate = true;
+				}
+		}
+			
+		return true;
 	}
 	
 }
