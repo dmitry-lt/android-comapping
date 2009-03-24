ed2fb33a05557cc133a15537822ffeddc0ccc076
korshakov.stepan <korshakov.stepan@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-03-24 21:00:11 +0000

    Added some lazy calculations in MapRender. It's added ~ 8 FPS.

--- project/src/com/comapping/android/model/MapRender.java
+++ project/src/com/comapping/android/model/MapRender.java
@@ -1,7 +1,7 @@
 /*
  * TestMapView Controller
  * Android Comapping, 2009
- * Last change: Korshakov Stepan
+ * Autor: Korshakov Stepan
  * 
  * Render of map in comapping.com style
  */
@@ -38,47 +38,60 @@ public class MapRender {
 				return "";
 		}
 		
-		public int getRowCount()
-		{
-			return  topicData.getText().length()/ROW_LENGTH + 1;
+		private int lazyRowCount = -1;
+		public int getRowCount() {
+			if (lazyRowCount == -1)
+				lazyRowCount = topicData.getText().length() / ROW_LENGTH + 1;
+			return lazyRowCount;
 		}
-		
-		public int getWidth()
-		{
-			Paint p = new Paint();
-			Rect bounds = new Rect();
-			
-			int linesCount = getRowCount();
-			int width = 0;
-			for(int i = 0; i < linesCount; i++)
-			{
-				String rowText = getRow(i);
-				p.getTextBounds(rowText, 0, rowText.length(), bounds);
-				width =Math.max(width, bounds.width());
-				//width += bounds.width();
-			}
-			return width + BORDER_SIZE*2;
-		}
-		
-		public int getHeight()
-		{
-			Paint p = new Paint();
-			Rect bounds = new Rect();
-			int linesCount = getRowCount();
-			int height = 0;
-			for(int i = 0; i < linesCount; i++)
-			{
-				String rowText = getRow(i);
-				p.getTextBounds(rowText, 0, rowText.length(), bounds);
 
-				height+=bounds.height();
+		private int lazyWidth = -1;
+		public int getWidth() {
+			if (lazyWidth == -1) {
+				Paint p = new Paint();
+				Rect bounds = new Rect();
+
+				int linesCount = getRowCount();
+				int width = 0;
+				for (int i = 0; i < linesCount; i++) {
+					String rowText = getRow(i);
+					p.getTextBounds(rowText, 0, rowText.length(), bounds);
+					width = Math.max(width, bounds.width());
+					// width += bounds.width();
+				}
+				lazyWidth = width + BORDER_SIZE * 2;
 			}
-			return height + UNDERLINE_OFFSET + BORDER_SIZE*2;
+			return lazyWidth;
 		}
 		
+		private int lazyHeight = -1;
+		public int getHeight() {
+			if (lazyWidth == -1) {
+				Paint p = new Paint();
+				Rect bounds = new Rect();
+				int linesCount = getRowCount();
+				int height = 0;
+				for (int i = 0; i < linesCount; i++) {
+					String rowText = getRow(i);
+					p.getTextBounds(rowText, 0, rowText.length(), bounds);
+
+					height += bounds.height();
+				}
+				lazyHeight = height + UNDERLINE_OFFSET + BORDER_SIZE * 2;
+			}
+			return lazyHeight;
+		}
+		
+		
+		private int lazyOffset = -1;
 		private int getOffset()
 		{
-			return (subtreeWidth-getHeight())/2; 
+			if (lazyOffset == -1)
+			{
+				lazyOffset = (subtreeWidth-getHeight())/2;
+			}
+			
+			return lazyOffset;
 		}
 		
 		public void draw(int x, int y, Canvas c)
@@ -115,10 +128,16 @@ public class MapRender {
 			//c.drawText(text, x + BORDER_SIZE, y + vertOffset + bounds.height()+BORDER_SIZE, p);
 		}
 		
+		private int lazyUnderlineOffset = -1;
 		public int getUnderlineOffset()
 		{
-			return getHeight()+ getOffset() - BORDER_SIZE;
+			if (lazyUnderlineOffset == -1)
+				lazyUnderlineOffset = getHeight()+ getOffset() - BORDER_SIZE; 
+			
+			return lazyUnderlineOffset;
 		}
+	
+		
 	}
 	
 	
