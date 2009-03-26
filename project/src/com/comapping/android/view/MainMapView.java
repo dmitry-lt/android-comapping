f1388e8df30babd0ec41e65d31306fb0e83b91d5
korshakov.stepan <korshakov.stepan@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-03-26 15:56:03 +0000

    Fixed Taping

--- project/src/com/comapping/android/view/MainMapView.java
+++ project/src/com/comapping/android/view/MainMapView.java
@@ -13,46 +13,52 @@ public class MainMapView extends View {
 
 	Render mRender;
 	Scroller mScroller;
-	
+
 	public MainMapView(Context context, Render render) {
 		super(context);
 		mRender = render;
 		mScroller = new Scroller(context);
 	}
-	
+
 	int frameCount = 0;
 	long fps = 0;
 	long lastFPSCalcTime = System.currentTimeMillis();
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		mScroller.computeScrollOffset();
-		
+
 		Paint p = new Paint();
 		canvas.drawARGB(255, 255, 255, 255);
-		
-		mRender.draw(mScroller.getCurrX(), mScroller.getCurrY(), this.getWidth(), this.getHeight(), canvas);
-		
+
+		mRender.draw(mScroller.getCurrX(), mScroller.getCurrY(), this
+				.getWidth(), this.getHeight(), canvas);
+
 		p.setColor(Color.BLACK);
 		canvas.drawText("FPS: " + fps, 20, 30, p);
 		if (System.currentTimeMillis() - lastFPSCalcTime > 1000) {
-			fps = (1000 * frameCount) / (System.currentTimeMillis() - lastFPSCalcTime);
+			fps = (1000 * frameCount)
+					/ (System.currentTimeMillis() - lastFPSCalcTime);
 			lastFPSCalcTime = System.currentTimeMillis();
 			frameCount = 0;
 		}
 		frameCount++;
 		invalidate();
 	}
-	
+
 	VelocityTracker mVelocityTracker;
 	int oldX = -1, oldY = -1;
 	long touchStartTime = 0;
+
+	private static final long TAP_MAX_TIME = 100;
+
 	@Override
 	public boolean onTouchEvent(MotionEvent ev) {
-		
+
 		int action = ev.getAction();
 		switch (action) {
 		case MotionEvent.ACTION_DOWN: {
-			mRender.onTouch((int)ev.getX(), (int)ev.getY());
+			// mRender.onTouch((int)ev.getX(), (int)ev.getY());
 			mVelocityTracker = VelocityTracker.obtain();
 			oldX = (int) ev.getX();
 			oldY = (int) ev.getY();
@@ -67,25 +73,34 @@ public class MainMapView extends View {
 			oldX = (int) ev.getX();
 			oldY = (int) ev.getY();
 
-			mScroller.startScroll(mScroller.getCurrX(), mScroller.getCurrY(), deltaX, deltaY, 0);
+			mScroller.startScroll(mScroller.getCurrX(), mScroller.getCurrY(),
+					deltaX, deltaY, 0);
 			return true;
 		}
 		case MotionEvent.ACTION_UP: {
-			mVelocityTracker.addMovement(ev);
+			if (System.currentTimeMillis() - touchStartTime < TAP_MAX_TIME) {
+				mRender.onTouch(mScroller.getCurrX() + (int)ev.getX(), 
+						mScroller.getCurrY() + (int)ev.getY());
+			} else {
+				mVelocityTracker.addMovement(ev);
 
-			mVelocityTracker.computeCurrentVelocity(1000);
+				mVelocityTracker.computeCurrentVelocity(1000);
 
-			int vx = (int) mVelocityTracker.getXVelocity();
-			int vy = (int) mVelocityTracker.getYVelocity();
+				int vx = (int) mVelocityTracker.getXVelocity();
+				int vy = (int) mVelocityTracker.getYVelocity();
 
-			vx = vx * 3 / 4;
-			vy = vy * 3 / 4;
+				vx = vx * 3 / 4;
+				vy = vy * 3 / 4;
 
-			mScroller.fling(mScroller.getCurrX(), mScroller.getCurrY(), -vx, -vy, 0, mRender.getWidth(), 0, mRender.getHeight());
-			
-			mVelocityTracker.recycle();
+				mScroller
+						.fling(mScroller.getCurrX(), mScroller.getCurrY(), -vx,
+								-vy, 0, mRender.getWidth(), 0, mRender
+										.getHeight());
 
-			return true;
+				mVelocityTracker.recycle();
+
+				return true;
+			}
 		}
 
 		}
