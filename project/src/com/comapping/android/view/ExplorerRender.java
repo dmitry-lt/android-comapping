d28822aec7c233950e940446097c299f829ef38f
Vldimir.Kulikov <Vldimir.Kulikov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-04-02 15:04:15 +0000

    Add lazy calculations

--- project/src/com/comapping/android/view/ExplorerRender.java
+++ project/src/com/comapping/android/view/ExplorerRender.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 public class ExplorerRender extends Render {
 
@@ -37,6 +38,9 @@ public class ExplorerRender extends Render {
 
 	private boolean toUpdate = true;
 	private ArrayList<touchPoint> points = new ArrayList<touchPoint>();
+	private HashMap<Integer, Integer> topicX = new HashMap<Integer, Integer>();
+	private HashMap<Integer, Integer> topicY = new HashMap<Integer, Integer>();
+	private ArrayList<Rect> lines = new ArrayList<Rect>();
 	private int xPlus, yPlus;
 	private int height, width;
 	private int screenWidth, screenHeight;
@@ -58,7 +62,50 @@ public class ExplorerRender extends Render {
 				&& intersects(0, screenHeight, y1, y2);
 	}
 
-	private int[] drawTopic(Topic topic, int x, int y, Canvas c) {
+	private void drawTopics(Canvas c) {
+		Paint p = new Paint();
+
+		// draw lines
+		p.setColor(Color.GRAY);
+		for (Rect line : lines) {
+			int x1 = line.left + xPlus;
+			int y1 = line.top + yPlus;
+			int x2 = line.right + xPlus;
+			int y2 = line.bottom + yPlus;
+			if (onScreen(x1, y1, x2, y2))
+				c.drawLine(x1, y1, x2, y2, p);
+		}
+
+		for (touchPoint point : points) {
+			// draw circles
+			int x = point.x + xPlus;
+			int y = point.y + yPlus;
+			int id = point.id;
+			if (open.get(id) != null
+					&& onScreen(x - OUTER_SIZE, y - OUTER_SIZE, x + OUTER_SIZE,
+							y + OUTER_SIZE)) {
+				c.drawCircle(x, y, OUTER_SIZE, p);
+				p.setColor(Color.WHITE);
+				c.drawCircle(x, y, OUTER_SIZE - CIRCLE_WIDTH, p);
+				p.setColor(Color.GRAY);
+				c.drawRect(x - PLUS_LENGTH, y - PLUS_WIDTH, x + PLUS_LENGTH, y
+						+ PLUS_WIDTH, p);
+				if (!open.get(id))
+					c.drawRect(x - PLUS_WIDTH, y - PLUS_LENGTH, x + PLUS_WIDTH,
+							y + PLUS_LENGTH, p);
+			}
+			// draw topics
+			x = topicX.get(id) + xPlus;
+			y = topicY.get(id) + yPlus;
+			TopicRender topicRender = topicRenders.get(id);
+			if (onScreen(x, y, x + topicRender.getWidth(), y
+					+ topicRender.getHeight()))
+				topicRender.draw(x, y, 0, 0, c);
+		}
+
+	}
+
+	private int[] updateTopic(Topic topic, int x, int y) {
 		if (topic == null) {
 			return new int[3];
 		}
@@ -77,61 +124,39 @@ public class ExplorerRender extends Render {
 		x += OUTER_SIZE;
 		y += (height - topicRender.getHeight()) / 2;
 		x += X_SHIFT + BLOCK_SHIFT;
-		if (onScreen(x, y, x + topicRender.getWidth(), y
-				+ topicRender.getHeight()))
-			topicRender.draw(x, y, 0, 0, c);
+		topicX.put(topic.getId(), x);
+		topicY.put(topic.getId(), y);
 		x -= X_SHIFT + BLOCK_SHIFT;
 
-		// draw circle and line
+		// update circle and line
 		y = ret[1] + ret[2];
-		Paint p = new Paint();
-		p.setColor(Color.GRAY);
-		if (onScreen(x, y, x + X_SHIFT, y))
-			c.drawLine(x, y, x + X_SHIFT, y, p);
+		lines.add(new Rect(x, y, x + X_SHIFT, y));
 		if (topic.getChildrenCount() > 0) {
 			if (open.get(topic.getId()) == null)
 				open.put(topic.getId(), true);
-			if (toUpdate)
-				points.add(new touchPoint(x - xPlus, y - yPlus, topic.getId()));
-			if (onScreen(x - OUTER_SIZE, y - OUTER_SIZE, x + OUTER_SIZE, y
-					+ OUTER_SIZE)) {
-				c.drawCircle(x, y, OUTER_SIZE, p);
-				p.setColor(Color.WHITE);
-				c.drawCircle(x, y, OUTER_SIZE - CIRCLE_WIDTH, p);
-				p.setColor(Color.GRAY);
-				c.drawRect(x - PLUS_LENGTH, y - PLUS_WIDTH, x + PLUS_LENGTH, y
-						+ PLUS_WIDTH, p);
-				if (!open.get(topic.getId()))
-					c.drawRect(x - PLUS_WIDTH, y - PLUS_LENGTH, x + PLUS_WIDTH,
-							y + PLUS_LENGTH, p);
-			}
 		}
+		points.add(new touchPoint(x, y, topic.getId()));
 
-		// draw subtopics
+		// update subtopics
 		x += X_SHIFT;
 		y += ret[2];
 		if (topic.getChildrenCount() > 0 && open.get(topic.getId())) {
 			int py = y - ret[2];
-			p.setColor(Color.GRAY);
 			int[] temp;
-			if (y <= screenHeight) {
-				for (int i = 0; i < topic.getChildrenCount(); i++) {
-					y += Y_SHIFT;
-					temp = drawTopic(topic.getChildByIndex(i), x - OUTER_SIZE,
-							y, c);
+			for (int i = 0; i < topic.getChildrenCount(); i++) {
+				y += Y_SHIFT;
+				temp = updateTopic(topic.getChildByIndex(i), x - OUTER_SIZE, y);
 
-					int ny = y + temp[2];
-					if (topic.getChildByIndex(i).getChildrenCount() > 0)
-						ny -= OUTER_SIZE;
-					if (onScreen(x, py, x, ny))
-						c.drawLine(x, py, x, ny, p);
-					py = y + temp[2];
-					if (topic.getChildByIndex(i).getChildrenCount() > 0)
-						py += OUTER_SIZE;
+				int ny = y + temp[2];
+				if (topic.getChildByIndex(i).getChildrenCount() > 0)
+					ny -= OUTER_SIZE;
+				lines.add(new Rect(x, py, x, ny));
+				py = y + temp[2];
+				if (topic.getChildByIndex(i).getChildrenCount() > 0)
+					py += OUTER_SIZE;
 
-					ret[0] = Math.max(ret[0], temp[0] - OUTER_SIZE);
-					y += temp[1];
-				}
+				ret[0] = Math.max(ret[0], temp[0] - OUTER_SIZE);
+				y += temp[1];
 			}
 		}
 
@@ -149,12 +174,15 @@ public class ExplorerRender extends Render {
 		yPlus = y;
 		screenWidth = width;
 		screenHeight = height;
-		if (toUpdate)
+		if (toUpdate) {
 			points.clear();
-		int[] temp = drawTopic(map.getRoot(), x, y, c);
-		this.width = temp[0];
-		this.height = temp[1];
-		toUpdate = false;
+			lines.clear();
+			int[] temp = updateTopic(map.getRoot(), 0, 0);
+			this.width = temp[0];
+			this.height = temp[1];
+			toUpdate = false;
+		}
+		drawTopics(c);
 	}
 
 	@Override
@@ -171,8 +199,10 @@ public class ExplorerRender extends Render {
 	public void onTouch(int x, int y) {
 		for (touchPoint point : points) {
 			if (Math.hypot(point.x + xPlus - x, point.y + yPlus - y) <= OUTER_SIZE) {
-				open.put(point.id, !open.get(point.id));
-				toUpdate = true;
+				if (open.get(point.id) != null) {
+					open.put(point.id, !open.get(point.id));
+					toUpdate = true;
+				}
 				break;
 			}
 		}
