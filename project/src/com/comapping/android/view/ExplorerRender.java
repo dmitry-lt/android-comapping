42b97e0d400ad6cd7ff455f8002d3fcb5c4b4e9f
Vldimir.Kulikov <Vldimir.Kulikov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-04-02 11:31:44 +0000

    Add TopicRender

--- project/src/com/comapping/android/view/ExplorerRender.java
+++ project/src/com/comapping/android/view/ExplorerRender.java
@@ -13,149 +13,108 @@ import android.graphics.Paint;
 
 public class ExplorerRender extends Render {
 
-	private class touchPoint
-	{
+	private class touchPoint {
 		public int x, y, id;
-		
-		public touchPoint(int x, int y, int id)
-		{
+
+		public touchPoint(int x, int y, int id) {
 			this.x = x;
 			this.y = y;
 			this.id = id;
 		}
 	}
-	
-	private static final int xShift = 30;
-	private static final int yShift = 15;
-	private static final int outerSize = 10;
-	private static final int innerSize = 5;
-	private static final int borderSize = 4;
-	private static final int blockShift = 5;
-	
+
+	private static final int X_SHIFT = 30;
+	private static final int Y_SHIFT = 15;
+	private static final int OUTER_SIZE = 15;
+	private static final int CIRCLE_WIDTH = 2;
+	private static final int PLUS_LENGTH = 7;
+	private static final int PLUS_WIDTH = 2;
+	private static final int BLOCK_SHIFT = 5;
+
 	private Map map;
 	private HashMap<Integer, Boolean> open = new HashMap<Integer, Boolean>();
-	private HashMap<Integer, TextRender> renders = new HashMap<Integer, TextRender>();
-	private HashMap<Integer, IconRender> renders1 = new HashMap<Integer, IconRender>();
+	private HashMap<Integer, TopicRender> topicRenders = new HashMap<Integer, TopicRender>();
 
 	private boolean toUpdate = true;
 	private ArrayList<touchPoint> points = new ArrayList<touchPoint>();
 	private int xPlus, yPlus;
 	private int height, width;
-	
-	public ExplorerRender (Context context, Map map)
-	{
-		this.map = map;		
+
+	public ExplorerRender(Context context, Map map) {
+		this.map = map;
 	}
-	
-	private int[] drawTopicContext(Topic topic, int x, int y, Canvas c)
-	{
-		int[] ret = new int[2];
-		ret[0] = x;
-		
-		Paint p = new Paint();
-		
-		x += outerSize;
-		
-		//calculate sizes
-		if (renders.get(topic.getId()) == null)
-			renders.put(topic.getId(), new TextRender(topic.getFormattedText()));
-		if (renders1.get(topic.getId()) == null)
-			renders1.put(topic.getId(), new IconRender(topic));
-		TextRender render = renders.get(topic.getId());
-		IconRender render1 = renders1.get(topic.getId());
-		int xSize = render.getWidth(), ySize = render.getHeight();
-				
-		int ty = Math.max(outerSize / 2, ySize / 2 + borderSize);
-		ty = Math.max(ty, render1.getHeight() / 2);
-		ret[1] = ty * 2;
-		y += ty;
-		
-		//draw circle and line
-		p.setColor(Color.GRAY);
-		c.drawLine(x, y, x + xShift, y, p);
+
+	private int[] drawTopic(Topic topic, int x, int y, Canvas c) {
+		if (topic == null) {
+			return new int[3];
+		}
+
+		if (topicRenders.get(topic.getId()) == null)
+			topicRenders.put(topic.getId(), new TopicRender(topic));
+		TopicRender topicRender = topicRenders.get(topic.getId());
+		int height = topicRender.getHeight();
 		if (topic.getChildrenCount() > 0)
-		{
+			height = Math.max(height, OUTER_SIZE * 2);
+		int[] ret = new int[3];
+		ret[0] = topicRender.getWidth() + BLOCK_SHIFT;
+		ret[1] = y;
+		ret[2] = height / 2;
+
+		x += OUTER_SIZE;
+		y += (height - topicRender.getHeight()) / 2;
+		topicRender.draw(x + X_SHIFT + BLOCK_SHIFT, y, 0, 0, c);
+
+		// draw circle and line
+		y = ret[1] + ret[2];
+		Paint p = new Paint();
+		p.setColor(Color.GRAY);
+		c.drawLine(x, y, x + X_SHIFT, y, p);
+		if (topic.getChildrenCount() > 0) {
 			if (open.get(topic.getId()) == null)
 				open.put(topic.getId(), true);
-			c.drawCircle(x, y, outerSize, p);
+			c.drawCircle(x, y, OUTER_SIZE, p);
+			p.setColor(Color.WHITE);
+			c.drawCircle(x, y, OUTER_SIZE - CIRCLE_WIDTH, p);
+			p.setColor(Color.GRAY);
 			if (toUpdate)
 				points.add(new touchPoint(x - xPlus, y - yPlus, topic.getId()));
-			p.setColor(Color.BLACK);
-			c.drawLine(x - innerSize, y, x + innerSize, y, p);
+			c.drawRect(x - PLUS_LENGTH, y - PLUS_WIDTH, x + PLUS_LENGTH, y
+					+ PLUS_WIDTH, p);
 			if (!open.get(topic.getId()))
-				c.drawLine(x, y - innerSize, x, y + innerSize, p);
+				c.drawRect(x - PLUS_WIDTH, y - PLUS_LENGTH, x + PLUS_WIDTH, y
+						+ PLUS_LENGTH, p);
 		}
-		x += xShift;
-		
-		//draw icons
-		x += blockShift;
-		render1.draw(x, y - render1.getHeight() / 2, 0, 0, c);
-		x += render1.getWidth();
 
-		//draw text		
-		x += blockShift;
-		p.setColor(topic.getBgColor());
-		p.setAlpha(255);
-		c.drawRect(x, y - ySize / 2 - borderSize, x + xSize + borderSize * 2, y + ySize / 2 + borderSize, p);
-		
-		x += borderSize;
-		
-		render.draw(x, y - ySize / 2, 0, 0, c);
-		
-		x += render.getWidth() + borderSize;
-		ret[0] = x - ret[0];
-		
-		return ret;
-	}
-	
-	private int[] drawTopic(Topic topic, int x, int y, Canvas c)
-	{
-		if (topic == null)
-		{
-			return new int[2];
-		}
-		
-		int[] temp = drawTopicContext(topic, x, y, c);		
-		int[] ret = new int[3];
-		ret[0] = temp[0];
-		ret[1] = y;
-		ret[2] = temp[1] / 2;
-		x += xShift;
-		y += temp[1];
-		
-		if (topic.getChildrenCount() > 0 && open.get(topic.getId()))
-		{
-			int flag = 0;
+		// draw subtopics
+		x += X_SHIFT;
+		y += ret[2];
+		if (topic.getChildrenCount() > 0 && open.get(topic.getId())) {
 			int py = y - ret[2];
-			Paint p = new Paint();
 			p.setColor(Color.GRAY);
-			for (int i = 0; i < topic.getChildrenCount(); i++)
-			{
-				y += yShift;
-				temp = drawTopic(topic.getChildByIndex(i), x, y, c);
+			int[] temp;
+			for (int i = 0; i < topic.getChildrenCount(); i++) {
+				y += Y_SHIFT;
+				temp = drawTopic(topic.getChildByIndex(i), x - OUTER_SIZE, y, c);
 
-				if (flag == 1)
-					py += outerSize;
 				int ny = y + temp[2];
 				if (topic.getChildByIndex(i).getChildrenCount() > 0)
-					ny -= outerSize;
-				c.drawLine(x + outerSize, py, x + outerSize, ny, p);
-				py = y + temp[2]; 
+					ny -= OUTER_SIZE;
+				c.drawLine(x, py, x, ny, p);
+				py = y + temp[2];
 				if (topic.getChildByIndex(i).getChildrenCount() > 0)
-					flag = 1;
-				else
-					flag = 0;
-				
-				ret[0] = Math.max(ret[0], temp[0] + xShift);
+					py += OUTER_SIZE;
+
+				ret[0] = Math.max(ret[0], temp[0] - OUTER_SIZE);
 				y += temp[1];
 			}
 		}
-		
+
+		ret[0] += X_SHIFT + OUTER_SIZE;
 		ret[1] = y - ret[1];
-		
+
 		return ret;
 	}
-	
+
 	@Override
 	public void draw(int x, int y, int width, int height, Canvas c) {
 		x = -x;
@@ -168,17 +127,7 @@ public class ExplorerRender extends Render {
 		this.width = temp[0];
 		this.height = temp[1];
 		toUpdate = false;
-		
-		/*Paint p = new Paint();
-		p.setColor(Color.BLACK);
-		c.drawLine(x + temp[0], y, x + temp[0], y + temp[1], p);
-		c.drawLine(x, y + temp[1], x + temp[0], y + temp[1], p);*/
-		
-		/*p.setColor(Color.RED);
-		for (touchPoint point : points)
-		{
-			c.drawCircle(point.x + xPlus, point.y + yPlus, 2, p);
-		}*/
+
 	}
 
 	@Override
@@ -193,10 +142,8 @@ public class ExplorerRender extends Render {
 
 	@Override
 	public void onTouch(int x, int y) {
-		for (touchPoint point : points)
-		{
-			if (Math.hypot(point.x + xPlus - x, point.y + yPlus - y) <= outerSize)
-			{
+		for (touchPoint point : points) {
+			if (Math.hypot(point.x + xPlus - x, point.y + yPlus - y) <= OUTER_SIZE) {
 				open.put(point.id, !open.get(point.id));
 				toUpdate = true;
 				break;
---
+++ project/src/com/comapping/android/view/TopicRender.java
@@ -0,0 +1,71 @@
+package com.comapping.android.view;
+
+import com.comapping.android.model.Topic;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class TopicRender extends Render {
+
+	private static final int HORISONTAL_MERGING = 5;
+	private static final int BORDER_SIZE = 4;
+
+	private IconRender iconRender;
+	private TextRender textRender;
+	private Topic topic;
+	private int width, height;
+
+	public TopicRender(Topic topic) {
+		this.topic = topic;
+		textRender = new TextRender(topic.getFormattedText());
+		iconRender = new IconRender(topic);
+
+		height = Math.max(iconRender.getHeight(), textRender.getHeight()
+				+ BORDER_SIZE * 2);
+		width = iconRender.getWidth() + HORISONTAL_MERGING
+				+ textRender.getWidth() + BORDER_SIZE * 2;
+	}
+
+	@Override
+	public void draw(int x, int y, int width, int height, Canvas c) {
+
+		Paint p = new Paint();
+
+		y += this.height / 2;
+
+		// draw icons
+		iconRender.draw(x, y - iconRender.getHeight() / 2, 0, 0, c);
+		x += iconRender.getWidth();
+
+		// draw text
+		x += HORISONTAL_MERGING;
+		p.setColor(topic.getBgColor());
+		p.setAlpha(255);
+		c.drawRect(x, y - textRender.getHeight() / 2 - BORDER_SIZE, x
+				+ textRender.getWidth() + BORDER_SIZE * 2, y
+				+ textRender.getHeight() / 2 + BORDER_SIZE, p);
+
+		x += BORDER_SIZE;
+
+		textRender.draw(x, y - textRender.getHeight() / 2, 0, 0, c);
+
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public void onTouch(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
