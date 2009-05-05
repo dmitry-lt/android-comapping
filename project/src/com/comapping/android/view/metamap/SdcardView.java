c15b23afb02006beedc5e074a6c5997118ec394b
abishev.timur <abishev.timur@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-05-05 10:27:44 +0000

    Added folder to sdcard view

--- project/src/com/comapping/android/view/metamap/SdcardView.java
+++ project/src/com/comapping/android/view/metamap/SdcardView.java
@@ -15,10 +15,11 @@ import com.comapping.android.model.exceptions.StringToXMLConvertionException;
 public class SdcardView extends MetaMapView {
 	final static private FilenameFilter filter = new FilenameFilter() {
 		public boolean accept(File dir, String filename) {
-			return filename.toLowerCase().endsWith(".comap");
+			return (new File(dir.getAbsolutePath() + File.pathSeparator + filename)).isDirectory()
+					|| filename.toLowerCase().endsWith(".comap");
 		}
 	};
-	
+
 	private static Map getSdMap() {
 		Map sdMap = new Map(0);
 		Topic root = new Topic(0, null);
@@ -33,19 +34,19 @@ public class SdcardView extends MetaMapView {
 		root.setNote("/sdcard");
 
 		sdMap.setRoot(root);
-		
+
 		return sdMap;
 	}
-	
+
 	public SdcardView() {
 		super(getSdMap());
 	}
-	
+
 	public void prepareTopic(Topic topic) {
 		topic.removeAllChildTopics();
 
 		File directory = new File(topic.getNote());
-		
+
 		for (File file : directory.listFiles(filter)) {
 			Topic newTopic = new Topic(0, topic);
 			try {
@@ -63,15 +64,15 @@ public class SdcardView extends MetaMapView {
 			topic.addChild(newTopic);
 		}
 	}
-	
+
 	public void activate(MetaMapActivity _metaMapActivity) {
 		super.activate(_metaMapActivity);
-		
+
 		drawMetaMap();
 		// change button to switch
 		ImageButton switchButton = (ImageButton) metaMapActivity.findViewById(R.id.viewSwitcher);
 		switchButton.setImageResource(R.drawable.internet_icon);
-		
+
 		// deactivate synchronize button
 		ImageButton synchronizeButton = (ImageButton) metaMapActivity.findViewById(R.id.synchronizeButton);
 		synchronizeButton.setEnabled(false);
