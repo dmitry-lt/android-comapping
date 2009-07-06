bc6d08125125e052b2d9b1179cbd1472deb1c818
passichenko.victor <passichenko.victor@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-06 07:22:37 +0000

    Add MapProviders based on ContentProvider

---
+++ project/src/com/comapping/android/provider/MapProvider.java
@@ -0,0 +1,87 @@
+package com.comapping.android.provider;
+
+import android.app.Activity;
+import android.content.ContentProvider;
+import android.database.AbstractCursor;
+import android.database.Cursor;
+import android.net.Uri;
+
+public abstract class MapProvider extends ContentProvider {
+	public static final String ID = "id";
+	public static final String TEXT = "content";
+	
+	public static Activity currentContext;
+	
+	public static String getComap(String mapId, Uri providerUri, Activity context) {
+		currentContext = context;
+		
+		Uri fullUri = Uri.withAppendedPath(providerUri, mapId);
+		Cursor cursor = context.managedQuery(fullUri, null, null, null, null);
+				
+		return cursor.getString(cursor.getColumnIndex(TEXT));
+	}
+	
+	abstract class MapCursor extends AbstractCursor {
+		protected String id;
+		protected String text;
+		
+		@Override
+		public String[] getColumnNames() {
+			return new String[] { ID, TEXT };
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+
+		@Override
+		public double getDouble(int column) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public float getFloat(int column) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int getInt(int column) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public long getLong(int column) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public short getShort(int column) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public String getString(int column) {
+			switch (column) {
+				case 0: 
+					return id;
+				case 1:
+					return text;
+				default:
+					throw new IllegalArgumentException("No such column " + column);
+			}
+		}
+
+		@Override
+		public boolean isNull(int column) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		
+	}
+}
