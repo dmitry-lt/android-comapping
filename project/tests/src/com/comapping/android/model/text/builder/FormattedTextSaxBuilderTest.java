79ab05172cfd9e5ab0e7703e3e1b8c53f2d6127d
yuri.zemlyanskiy <yuri.zemlyanskiy@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-08 10:22:43 +0000

    add unit tests

--- project/tests/src/com/comapping/android/model/text/builder/FormattedTextSaxBuilderTest.java
+++ project/tests/src/com/comapping/android/model/text/builder/FormattedTextSaxBuilderTest.java
@@ -1,15 +1,26 @@
 package com.comapping.android.model.text.builder;
 
+import java.util.List;
+
 import com.comapping.android.model.exceptions.StringToXMLConvertionException;
+import com.comapping.android.model.text.FormattedText;
+import com.comapping.android.model.text.TextBlock;
+import com.comapping.android.model.text.TextFormat;
+import com.comapping.android.model.text.TextParagraph;
 
 import android.test.AndroidTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
 public class FormattedTextSaxBuilderTest extends AndroidTestCase {
-
+	
+	@SmallTest
+	public void test0() throws StringToXMLConvertionException {
+		assertEquals("Error while parsing empty text", "", FormattedTextSaxBuilder.buildFormattedText("").getSimpleText());
+	}
+	
 	@SuppressWarnings("static-access")
 	@SmallTest
-	public void TestFormattedText() throws StringToXMLConvertionException {
+	public void test1() throws StringToXMLConvertionException {
 		String text = "Hello world!";
 		String[] xml = new String[] {
 				"<TEXT><P><FONT>Hello world!</FONT></P></TEXT>",
@@ -21,5 +32,19 @@ public class FormattedTextSaxBuilderTest extends AndroidTestCase {
 		assertEquals(text,builder.buildFormattedText(xml[1]).toString());
 		assertEquals(text,builder.buildFormattedText(xml[2]).toString());
 		assertEquals(text,builder.buildFormattedText(xml[3]).toString());
+	}
+	
+	@SmallTest
+	public void test2() throws StringToXMLConvertionException {
+		String xmlTestString = "<FONT SIZE=\"16\" COLOR=\"#000000\" LETTERSPACING=\"0\" KERNING=\"1\">Hello, world!</FONT>";
+		FormattedText formattedText = FormattedTextSaxBuilder.buildFormattedText(xmlTestString);
+		assertEquals("Error with parsing text", "Hello, world!", formattedText.getSimpleText());
+		List<TextParagraph> paragraphs = formattedText.getTextParagraphs();
+		assertEquals("Error with number of text paragraphs", 1, paragraphs.size());
+		assertEquals("Error with number of text blocks", 1, paragraphs.get(0).getTextBlocks().size());
+		TextFormat format = paragraphs.get(0).getTextBlocks().get(0).getFormat();
+		assertEquals("Error with format - size", 16, format.getFontSize());
+		assertEquals("Error with format - color", 0, format.getFontColor());
+		assertEquals("Error with format - underling", false, format.isUnderlined());
 	}
 }
