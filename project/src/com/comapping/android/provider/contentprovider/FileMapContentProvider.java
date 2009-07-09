fb7bebeccbdee4946d71e905e5917d2171647ee8
dmitriy.kozorez <dmitriy.kozorez@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-09 10:02:11 +0000

    FileMapContentProvider  implemented

--- project/src/com/comapping/android/provider/contentprovider/FileMapContentProvider.java
+++ project/src/com/comapping/android/provider/contentprovider/FileMapContentProvider.java
@@ -1,12 +1,39 @@
 package com.comapping.android.provider.contentprovider;
 
+import static com.comapping.android.provider.communication.ClientHelper.getBytesSum;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.comapping.android.Log;
+
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class FileMapContentProvider extends MapContentProvider {
+	public static final String PROVIDER_NAME = "sdcard";
 	public static final Uri CONTENT_URI = Uri
-			.parse("content://com.comapping.android.provider.filemapprovider");
+			.parse("content://" + PROVIDER_NAME);
+
+	public static final int PREFIX_LENGTH = 16;
+	
+	
+
+	public static boolean ignoreCache = true;
+
+	private static final int MAP = 1;
+	private static final int META_MAP = 2;
+	private static final UriMatcher uriMatcher;
+	static {
+		uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(PROVIDER_NAME, "/#####", MAP);
+		uriMatcher.addURI(PROVIDER_NAME, "/meta", META_MAP);
+	}
 
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -32,11 +59,16 @@ public class FileMapContentProvider extends MapContentProvider {
 		return false;
 	}
 
+	private String URiToId(Uri uri) {
+		
+		return uri.getPath().substring(PREFIX_LENGTH);
+	}
+
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
+		return new FileMapCursor(URiToId(uri));
+
 	}
 
 	@Override
@@ -44,6 +76,56 @@ public class FileMapContentProvider extends MapContentProvider {
 			String[] selectionArgs) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	private class FileMapCursor extends MapCursor {
+
+		protected String getTextFromInputStream(InputStream input)
+				throws IOException {
+			StringBuffer content = new StringBuffer();
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					input), 8 * 1024);
+
+			String line = null;
+			boolean first = true;
+
+			while ((line = reader.readLine()) != null) {
+				if (!first) {
+					content.append(System.getProperty("line.separator"));
+				} else {
+					first = false;
+				}
+
+				content.append(line);
+			}
+
+			reader.close();
+
+			return content.toString();
+		}
+
+		public FileMapCursor(String mapId) {
+			this.id = mapId;
+
+			String response = null;
+
+			try {
+				response = getTextFromInputStream(new FileInputStream(mapId));
+			} catch (FileNotFoundException e) {
+				Log.e(Log.CONNECTION_TAG, "map file not found");
+			} catch (IOException e) {
+				Log.e(Log.CONNECTION_TAG, "map file IO exception");
+			}
+
+			Log.d(Log.CONNECTION_TAG, "file comap provider response: "
+					+ response);
+			Log.d(Log.CONNECTION_TAG, "file comap provider check sum: "
+					+ getBytesSum(response));
+
+			this.text = response;
+
+		}
 	}
 
 }
