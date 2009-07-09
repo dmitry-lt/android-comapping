bb58037f8f4ddc8bca6ed307719bb2a6ebafe5bc
Dmitry.Manayev <Dmitry.Manayev@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-09 09:29:52 +0000

    back change,don't find the way of testing local files in Android..

--- project/tests/src/com/comapping/android/model/map/builder/MapBuilderTest.java
+++ project/tests/src/com/comapping/android/model/map/builder/MapBuilderTest.java
@@ -3,7 +3,6 @@ package com.comapping.android.model.map.builder;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
 import static com.comapping.android.provider.communication.ClientHelper.getTextFromInputStream;
 import com.comapping.android.model.exceptions.MapParsingException;
@@ -12,23 +11,14 @@ import com.comapping.android.model.map.Flag;
 import com.comapping.android.model.map.Map;
 import com.comapping.android.model.map.Topic;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
 
 public class MapBuilderTest extends AndroidTestCase {
 	
-	private Context context;
-	@SmallTest
 	public void test1() throws StringToXMLConvertionException, MapParsingException, FileNotFoundException, IOException
 	{
 		MapBuilder mapBuilder = new SaxMapBuilder();
-		try
-		{
-		Resources r = context.getResources();
-		InputStream stream = r.openRawResource(com.comapping.android.tests.R.raw.test1);//your project place of R file
-		Map map = mapBuilder.buildMap(getTextFromInputStream(stream));
+		Map map = mapBuilder.buildMap(getTextFromInputStream(new FileInputStream("sdcard\\test1.comap")));
 		Map mapE = new Map(61975);
 		Topic topic1 = new Topic(null);
 		topic1.setText("test1");
@@ -38,7 +28,6 @@ public class MapBuilderTest extends AndroidTestCase {
 		mapE.setRoot(topic1);
 		topic1.addChild(topic2);
 		assertTrue(map.simpleEquals(mapE));
-		}catch(NullPointerException ex){};
 	}
 	
 }
