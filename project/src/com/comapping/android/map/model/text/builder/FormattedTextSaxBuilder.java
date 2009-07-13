8ec2dc708fc780e0d60dd08024be24a7c80ad1d6
korshakov.stepan <korshakov.stepan@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-13 07:11:30 +0000

    Some precache for parsers

--- project/src/com/comapping/android/map/model/text/builder/FormattedTextSaxBuilder.java
+++ project/src/com/comapping/android/map/model/text/builder/FormattedTextSaxBuilder.java
@@ -25,20 +25,41 @@ public class FormattedTextSaxBuilder {
 	static final String FONT_ATTR_COLOR_TAG = "COLOR";
 	static final String HYPER_REF_TAG = "A";
 	static final String HYPER_REF_ATTR_HREF_TAG = "HREF";
-	static final String UNDERLINED_TAG = "U";	
-	
-	private static final String ERROR_TEXT = "#ERROR#"; 
-	
+	static final String UNDERLINED_TAG = "U";
+
+	private static final String ERROR_TEXT = "#ERROR#";
+
 	private static FormattedTextSaxHandler handler;
-	
+	private static SAXParserFactory saxParserFactory;
+	private static SAXParser parser;
+
+	private static void init() {
+		if (saxParserFactory == null)
+			saxParserFactory = SAXParserFactory.newInstance();
+
+		try {
+			if (parser == null)
+				parser = saxParserFactory.newSAXParser();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		handler = new FormattedTextSaxHandler();
+	}
+
 	static TextFormat getDefFormat() {
 		return new TextFormat();
 	}
 
-	public static FormattedText buildFormattedText(String xmlString) throws StringToXMLConvertionException {
-		
-//		long startTime = System.currentTimeMillis();
-		
+	public static FormattedText buildFormattedText(String xmlString)
+			throws StringToXMLConvertionException {
+
+		// long startTime = System.currentTimeMillis();
+
 		if (xmlString.startsWith("<P")) {
 			xmlString = "<TEXT>" + xmlString + "</TEXT>";
 		} else if (xmlString.startsWith("<FONT")) {
@@ -46,38 +67,39 @@ public class FormattedTextSaxBuilder {
 		} else {
 			xmlString = "<TEXT><P><FONT>" + xmlString + "</FONT></P></TEXT>";
 		}
-		
-		FormattedText resultText;
-		try {			
-			SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
-			SAXParser parser = saxParserFactory.newSAXParser();
 
-			InputStream stream = new ByteArrayInputStream(xmlString.getBytes("UTF-8"));
-			handler = new FormattedTextSaxHandler();
+		FormattedText resultText;
+		init();
+		try {
+			InputStream stream = new ByteArrayInputStream(xmlString
+					.getBytes("UTF-8"));
 
 			Log.d(Log.MODEL_TAG, "Text SAX parsing: " + xmlString);
-			
+
 			parser.parse(stream, handler);
 			resultText = handler.getFormattedText();
 		} catch (FactoryConfigurationError e) {
 			Log.e("SAX Text Parser", e.toString());
 			throw new DocumentBuilderCreatingError();
-		} catch (ParserConfigurationException e) {
-			Log.e("SAX Text Parser", e.toString());
-			throw new DocumentBuilderCreatingError();
 		} catch (SAXException e) {
-			Log.e(Log.MODEL_TAG, "cannot convert string to xml:" + e.toString());
+			Log
+					.e(Log.MODEL_TAG, "cannot convert string to xml:"
+							+ e.toString());
 			resultText = new FormattedText(ERROR_TEXT, getDefFormat());
-			//throw new StringToXMLConvertionException();
+			// throw new StringToXMLConvertionException();
 		} catch (IOException e) {
-			Log.e(Log.MODEL_TAG, "cannot convert string to xml:" + e.toString());
+			Log
+					.e(Log.MODEL_TAG, "cannot convert string to xml:"
+							+ e.toString());
 			throw new StringToXMLConvertionException();
 		}
 
-//		long parsingTime = System.currentTimeMillis() - startTime;
-		
-//		Log.d(Log.MODEL_TAG, "Formatted Text was built with SAX successfully, parsing time: " + parsingTime);
-		
+		// long parsingTime = System.currentTimeMillis() - startTime;
+
+		// Log.d(Log.MODEL_TAG,
+		// "Formatted Text was built with SAX successfully, parsing time: " +
+		// parsingTime);
+
 		return resultText;
 	}
 }
