686d1ea5a51efea4f4d962962b84c8a0f756a38c
yuri.zemlyanskiy <yuri.zemlyanskiy@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-15 10:24:18 +0000

    Now we can edit notes (only to test this feature)

--- project/src/com/comapping/android/map/render/topic/NoteRender.java
+++ project/src/com/comapping/android/map/render/topic/NoteRender.java
@@ -1,16 +1,26 @@
 package com.comapping.android.map.render.topic;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.comapping.android.Log;
+import com.comapping.android.controller.R;
 import com.comapping.android.map.model.text.FormattedText;
 import com.comapping.android.map.model.text.TextFormat;
 import com.comapping.android.map.render.Render;
 
 public class NoteRender extends Render {
-	private static final TextFormat FORMAT = new TextFormat(10, Color.GRAY, "", false);
+	private static final TextFormat FORMAT = new TextFormat(10, Color.GRAY, "",
+			false);
 	private static final int MIN_MAX_WIDTH = 100;
 	private static final int MAX_LINES_COUNT = 2;
 
@@ -30,7 +40,7 @@ public class NoteRender extends Render {
 		if (!isEmpty) {
 			this.note = note;
 			this.context = context;
-			
+
 			FormattedText text = new FormattedText(note, FORMAT);
 			textRender = new TextRender(text, context);
 			recalcDrawingData();
@@ -39,7 +49,6 @@ public class NoteRender extends Render {
 		}
 	}
 
-	
 	public void draw(int x, int y, int width, int height, Canvas c) {
 		if (!isEmpty) {
 			textRender.draw(x, y, width, height, c);
@@ -48,44 +57,66 @@ public class NoteRender extends Render {
 		}
 	}
 
-	
 	public int getHeight() {
 		return height;
 	}
 
-	
 	public int getWidth() {
 		return width;
 	}
 
-	
 	public void onTouch(int x, int y) {
 		if (!isEmpty) {
-			if (dialog == null) {
-				dialog = (new AlertDialog.Builder(context)
-				.setTitle("Note")
-				.setMessage(note)
-				.setNeutralButton("Ok", null)
-				).create();
-			}
+			
+			if (dialog == null)
+			{
+				LayoutInflater factory = LayoutInflater.from(context);
+				final View textEntryView = factory.inflate(com.comapping.android.controller.R.layout.topic_edit, null);
+				Log.d("NOTE REANDER !!!!!", "try to init dialobg view");
+            	((EditText)textEntryView.findViewById(R.id.topic_text_edit)).setText(note);
+            	dialog =  new AlertDialog.Builder(context)
+                .setIcon(R.drawable.metamap_map)
+                //.setTitle(R.string.alert_dialog_text_entry)
+                .setView(textEntryView)
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    	//Log.d("NOTE REANDER !!!!!", "Press save");
+                    	note = ((EditText)textEntryView.findViewById(R.id.topic_text_edit)).getText().toString();
+                    	
+            			FormattedText text = new FormattedText(note, FORMAT);
+            			textRender = new TextRender(text, context);
+            			recalcDrawingData();                    	
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
 
+                    	//Log.d("NOTE REANDER !!!!!", "press cancel");
+                        /* User clicked cancel so do some stuff */
+                    }
+                })    
+                .create();
+            	
+            	//Log.d("NOTE REANDER !!!!!", "was created...");
+			}
 			dialog.show();
 		}
 	}
 
 	public void setMaxWidth(int maxWidth) {
 		if (!isEmpty) {
-			//Log.d(Log.TOPIC_RENDER_TAG, "setting maxWidth=" + maxWidth + " in " + this);
-			textRender.setMaxWidthAndLinesCount(Math.max(maxWidth, MIN_MAX_WIDTH), MAX_LINES_COUNT);
+			// Log.d(Log.TOPIC_RENDER_TAG, "setting maxWidth=" + maxWidth +
+			// " in " + this);
+			textRender.setMaxWidthAndLinesCount(Math.max(maxWidth,
+					MIN_MAX_WIDTH), MAX_LINES_COUNT);
 			recalcDrawingData();
 		}
 	}
 
-	
 	public String toString() {
 		if (!isEmpty) {
-			return "[NoteRender: note=\"" + textRender.getSimpleText() + "\" width=" + getWidth() + " height="
-					+ getHeight() + "]";
+			return "[NoteRender: note=\"" + textRender.getSimpleText()
+					+ "\" width=" + getWidth() + " height=" + getHeight() + "]";
 		} else {
 			return "[NoteRender: EMPTY]";
 		}
