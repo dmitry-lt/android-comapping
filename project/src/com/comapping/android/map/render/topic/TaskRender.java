94a0a52e21b6f3c86b9aad9708a7373dd4c8f87c
Vldimir.Kulikov <Vldimir.Kulikov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-07-17 08:54:24 +0000

    Add estimate to task dialog

--- project/src/com/comapping/android/map/render/topic/TaskRender.java
+++ project/src/com/comapping/android/map/render/topic/TaskRender.java
@@ -44,19 +44,23 @@ public class TaskRender extends Render {
 			this.task = task;
 			this.context = context;
 
-			FormattedText responsible = new FormattedText(task.getResponsible(), FORMAT);
+			FormattedText responsible = new FormattedText(
+					task.getResponsible(), FORMAT);
 			responsibleRender = new TextRender(responsible, context);
 
-			FormattedText start = !task.getStart().equals("") ? new FormattedText("Start: " + task.getStart(), FORMAT)
+			FormattedText start = !task.getStart().equals("") ? new FormattedText(
+					"Start: " + task.getStart(), FORMAT)
 					: null;
 			startRender = new TextRender(start, context);
 
-			FormattedText deadline = !task.getDeadline().equals("") ? new FormattedText("Deadline: "
-					+ task.getDeadline(), FORMAT) : null;
+			FormattedText deadline = !task.getDeadline().equals("") ? new FormattedText(
+					"Deadline: " + task.getDeadline(), FORMAT)
+					: null;
 			deadlineRender = new TextRender(deadline, context);
-			
-			FormattedText estimate = !task.getEstimate().equals("") ? new FormattedText("Estimate: "
-					+ task.getEstimate(), FORMAT) : null;
+
+			FormattedText estimate = !task.getEstimate().equals("") ? new FormattedText(
+					"Estimate: " + task.getEstimate(), FORMAT)
+					: null;
 			estimateRender = new TextRender(estimate, context);
 
 			recalcDrawingData();
@@ -68,7 +72,8 @@ public class TaskRender extends Render {
 
 	public void setWidth(int width) {
 		if (!isEmpty) {
-			//Log.d(Log.TOPIC_RENDER_TAG, "setting width=" + width + " in " + this);
+			// Log.d(Log.TOPIC_RENDER_TAG, "setting width=" + width + " in " +
+			// this);
 
 			width = Math.max(width, MIN_MAX_WIDTH);
 
@@ -77,14 +82,18 @@ public class TaskRender extends Render {
 			deadlineRender.setMaxWidthAndLinesCount(width, 1);
 			estimateRender.setMaxWidthAndLinesCount(width, 1);
 
-			int sumWidth = responsibleRender.getWidth() + startRender.getWidth() + deadlineRender.getWidth();
-			if (!responsibleRender.isEmpty() && sumWidth + MIN_MERGING * 2 <= width) {
+			int sumWidth = responsibleRender.getWidth()
+					+ startRender.getWidth() + deadlineRender.getWidth();
+			if (!responsibleRender.isEmpty()
+					&& sumWidth + MIN_MERGING * 2 <= width) {
 				linesCount = 1;
 				merging = (width - sumWidth) / 2;
 			} else if (!startRender.isEmpty()
-					&& startRender.getWidth() + deadlineRender.getWidth() + MIN_MERGING <= width) {
+					&& startRender.getWidth() + deadlineRender.getWidth()
+							+ MIN_MERGING <= width) {
 				linesCount = 2;
-				merging = width - (startRender.getWidth() + deadlineRender.getWidth());
+				merging = width
+						- (startRender.getWidth() + deadlineRender.getWidth());
 			} else {
 				linesCount = 3;
 			}
@@ -93,46 +102,46 @@ public class TaskRender extends Render {
 		}
 	}
 
-	
 	public void draw(int x, int y, int width, int height, Canvas c) {
 		if (!isEmpty) {
-			responsibleRender.draw(x + responsibleCoords.x, y + responsibleCoords.y, 0, 0, c);
+			responsibleRender.draw(x + responsibleCoords.x, y
+					+ responsibleCoords.y, 0, 0, c);
 			startRender.draw(x + startCoords.x, y + startCoords.y, 0, 0, c);
-			deadlineRender.draw(x + deadlineCoords.x, y + deadlineCoords.y, 0, 0, c);
-			estimateRender.draw(x + estimateCoords.x, y + estimateCoords.y, 0, 0, c);
+			deadlineRender.draw(x + deadlineCoords.x, y + deadlineCoords.y, 0,
+					0, c);
+			estimateRender.draw(x + estimateCoords.x, y + estimateCoords.y, 0,
+					0, c);
 		} else {
 			// nothing to draw
 		}
 	}
 
-	
 	public String toString() {
 		if (!isEmpty) {
-			return "[TaskRender: width=" + getWidth() + " height=" + getHeight() + " linesCount=" + linesCount
-					+ "\n\t responsible=" + responsibleRender + "\n\t start=" + startRender + "\n\t deadline="
-					+ deadlineRender + "]\n";
+			return "[TaskRender: width=" + getWidth() + " height="
+					+ getHeight() + " linesCount=" + linesCount
+					+ "\n\t responsible=" + responsibleRender + "\n\t start="
+					+ startRender + "\n\t deadline=" + deadlineRender + "]\n";
 		} else {
 			return "[TaskRender: EMPTY]";
 		}
 	}
 
-	
 	public int getHeight() {
 		return height;
 	}
 
-	
 	public int getWidth() {
 		return width;
 	}
 
-	
 	public void onTouch(int x, int y) {
 		if (!isEmpty) {
 			if (dialog == null) {
 				dialog = (new AlertDialog.Builder(context).setTitle("Task").setMessage(
 						"Responsible: " + task.getResponsible() + "\n" + "Start date: " + task.getStart() + "\n" + "Deadline: "
-								+ task.getDeadline()).setNeutralButton("Ok", null)).create();
+								+ task.getDeadline() + "\n" + "Estimate: "
+								+ task.getEstimate()).setNeutralButton("Ok", null)).create();
 			}
 
 			dialog.show();
@@ -142,43 +151,54 @@ public class TaskRender extends Render {
 	private void recalcDrawingData() {
 		switch (linesCount) {
 		case 1: // 1: resp start deadline
-			height = Math.max(responsibleRender.getHeight(), startRender.getHeight());
+			height = Math.max(responsibleRender.getHeight(), startRender
+					.getHeight());
 			height = Math.max(height, deadlineRender.getHeight());
-			width = responsibleRender.getWidth() + startRender.getWidth() + deadlineRender.getWidth() + merging * 2;
+			width = responsibleRender.getWidth() + startRender.getWidth()
+					+ deadlineRender.getWidth() + merging * 2;
 
 			responsibleCoords = new Point(0, 0);
 			startCoords = new Point(responsibleRender.getWidth() + merging, 0);
 			deadlineCoords = new Point(width - deadlineRender.getWidth(), 0);
 			break;
 		case 2: // 1: resp 2: start deadline
-			responsibleRender.setBorder(TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER,
-					TextRender.DEFAULT_BORDER, 0);
-			startRender.setBorder(TextRender.DEFAULT_BORDER, 0, TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
-			deadlineRender
-					.setBorder(TextRender.DEFAULT_BORDER, 0, TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
+			responsibleRender.setBorder(TextRender.DEFAULT_BORDER,
+					TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER, 0);
+			startRender.setBorder(TextRender.DEFAULT_BORDER, 0,
+					TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
+			deadlineRender.setBorder(TextRender.DEFAULT_BORDER, 0,
+					TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
 
-			height = responsibleRender.getHeight() + Math.max(startRender.getHeight(), deadlineRender.getHeight());
-			width = Math
-					.max(responsibleRender.getWidth(), startRender.getWidth() + deadlineRender.getWidth() + merging);
+			height = responsibleRender.getHeight()
+					+ Math.max(startRender.getHeight(), deadlineRender
+							.getHeight());
+			width = Math.max(responsibleRender.getWidth(), startRender
+					.getWidth()
+					+ deadlineRender.getWidth() + merging);
 
 			responsibleCoords = new Point(0, 0);
 			startCoords = new Point(0, responsibleRender.getHeight());
-			deadlineCoords = new Point(width - deadlineRender.getWidth(), responsibleRender.getHeight());
+			deadlineCoords = new Point(width - deadlineRender.getWidth(),
+					responsibleRender.getHeight());
 			break;
 		case 3: // 1: resp 2: start 3: deadline
-			responsibleRender.setBorder(TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER,
+			responsibleRender.setBorder(TextRender.DEFAULT_BORDER,
+					TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER, 0);
+			startRender.setBorder(TextRender.DEFAULT_BORDER, 0,
 					TextRender.DEFAULT_BORDER, 0);
-			startRender.setBorder(TextRender.DEFAULT_BORDER, 0, TextRender.DEFAULT_BORDER, 0);
-			deadlineRender
-					.setBorder(TextRender.DEFAULT_BORDER, 0, TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
+			deadlineRender.setBorder(TextRender.DEFAULT_BORDER, 0,
+					TextRender.DEFAULT_BORDER, TextRender.DEFAULT_BORDER);
 
-			height = responsibleRender.getHeight() + startRender.getHeight() + deadlineRender.getHeight();
-			width = Math.max(responsibleRender.getWidth(), startRender.getWidth());
+			height = responsibleRender.getHeight() + startRender.getHeight()
+					+ deadlineRender.getHeight();
+			width = Math.max(responsibleRender.getWidth(), startRender
+					.getWidth());
 			width = Math.max(width, deadlineRender.getWidth());
 
 			responsibleCoords = new Point(0, 0);
 			startCoords = new Point(0, responsibleRender.getHeight());
-			deadlineCoords = new Point(0, responsibleRender.getHeight() + startRender.getHeight());
+			deadlineCoords = new Point(0, responsibleRender.getHeight()
+					+ startRender.getHeight());
 			break;
 		}
 		estimateCoords = new Point(0, height);
