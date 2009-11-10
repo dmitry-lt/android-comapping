53b5d4b372067d328f3f5a433e93fae431b87894
Fedor.Burdun <Fedor.Burdun@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-10 03:38:42 +0000

    Added tabs and view simply notifications.

--- notifer/project/com.comapping.android.test/src/com/comapping/android/test/MainActivity.java
+++ notifer/project/com.comapping.android.test/src/com/comapping/android/test/MainActivity.java
@@ -1,22 +1,25 @@
 // Written by Valery Petrov
+// Changed by Fedor Burdun (10.11.2009)
 
 package com.comapping.android.test;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity  extends TabActivity implements TabHost.TabContentFactory {
 	private static final String TAG = "MainActivity";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		/*
+		(setContentView(R.layout.main);
 		Log.d(TAG, "starting service");
 		Button bindBtn = (Button) findViewById(R.id.bindBtn);
 		bindBtn.setOnClickListener(new OnClickListener() {
@@ -34,5 +37,44 @@ public class MainActivity extends Activity {
 						BackgroundService.class));
 			}
 		});
+		*/
+		Log.d(TAG, "starting service");
+		startService(new Intent(MainActivity.this, BackgroundService.class));
+		
+		TabHost tabHost = getTabHost();
+
+		tabHost.addTab(tabHost.newTabSpec("Day").setIndicator("Day")
+				.setContent(this));
+		tabHost.addTab(tabHost.newTabSpec("Week").setIndicator("Week")
+				.setContent(this));
+		tabHost.addTab(tabHost.newTabSpec("All").setIndicator("All")
+				.setContent(this));
+		
 	}
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		stopService(new Intent(MainActivity.this, BackgroundService.class));
+	}
+	
+	@Override
+	public View createTabContent(String tag) {
+		// TODO Auto-generated method stub
+		ListView lv = new ListView(this);
+		
+		if (tag.equals("Day"))
+		lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+				new AbstractNotificationGenerator().getListNotification(5)));
+		else if (tag.equals("Week"))
+				lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+						new AbstractNotificationGenerator().getListNotification(10)));
+		else if (tag.equals("All"))
+			lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+					new AbstractNotificationGenerator().getListNotification(20)));
+
+		return lv;
+	}
+
 }
