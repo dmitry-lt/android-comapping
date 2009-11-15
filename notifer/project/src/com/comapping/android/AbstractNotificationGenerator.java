a49ea3060affabd9f83d509deee292812056de36
valerypetrov7@gmail.com <valerypetrov7@gmail.com@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-15 15:04:17 +0000

    Added more notifications with some random dates to check work

--- notifer/project/src/com/comapping/android/AbstractNotificationGenerator.java
+++ notifer/project/src/com/comapping/android/AbstractNotificationGenerator.java
@@ -1,4 +1,5 @@
 // Created by Fedor Burdun
+// Changed by Valery Petrov (15.11.2009)
 
 package com.comapping.android;
 
@@ -7,7 +8,12 @@ import java.util.ArrayList;
 public class AbstractNotificationGenerator {
 	public ArrayList<Notification> getListNotification(String date) {
 		ArrayList<Notification> al = new ArrayList<Notification>();
-		al.add(new Notification("t1","t2","t3","t4","t5"));
+		al.add(new Notification("10.10.10","Notification 10.10.10","t3","t4","t5"));
+		al.add(new Notification("11.10.10","Notification 11.10.10","t3","t4","t5"));
+		al.add(new Notification("12.10.10","Notification 12.10.10","t3","t4","t5"));
+		al.add(new Notification("15.10.10","Notification 15.10.10","t3","t4","t5"));
+		al.add(new Notification("18.10.10","Notification 18.10.10","t3","t4","t5"));
+		al.add(new Notification("19.10.10","Notification 19.10.10","t3","t4","t5"));
 		return al;
 	}
 }
