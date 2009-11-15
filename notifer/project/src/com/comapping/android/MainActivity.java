e06277bad936d2168021984cce75fc87ddba2d8c
valerypetrov7@gmail.com <valerypetrov7@gmail.com@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-15 15:05:36 +0000

    Added displaying of lists of notifications from AbstractNotificationGenerator grouped by tabs

--- notifer/project/src/com/comapping/android/MainActivity.java
+++ notifer/project/src/com/comapping/android/MainActivity.java
@@ -3,6 +3,11 @@
 
 package com.comapping.android;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Locale;
+
 import android.app.TabActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -12,35 +17,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
 
-public class MainActivity  extends TabActivity implements TabHost.TabContentFactory {
+public class MainActivity extends TabActivity implements
+		TabHost.TabContentFactory {
 	private static final String TAG = "MainActivity";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		/*
-		(setContentView(R.layout.main);
-		Log.d(TAG, "starting service");
-		Button bindBtn = (Button) findViewById(R.id.bindBtn);
-		bindBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				startService(new Intent(MainActivity.this,
-						BackgroundService.class));
-			}
-		});
-		Button unbindBtn = (Button) findViewById(R.id.unbindBtn);
-		unbindBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				stopService(new Intent(MainActivity.this,
-						BackgroundService.class));
-			}
-		});
-		*/
+		 * (setContentView(R.layout.main); Log.d(TAG, "starting service");
+		 * Button bindBtn = (Button) findViewById(R.id.bindBtn);
+		 * bindBtn.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View arg0) { startService(new
+		 * Intent(MainActivity.this, BackgroundService.class)); } }); Button
+		 * unbindBtn = (Button) findViewById(R.id.unbindBtn);
+		 * unbindBtn.setOnClickListener(new OnClickListener() {
+		 * 
+		 * @Override public void onClick(View arg0) { stopService(new
+		 * Intent(MainActivity.this, BackgroundService.class)); } });
+		 */
 		Log.d(TAG, "starting service");
 		startService(new Intent(MainActivity.this, BackgroundService.class));
-		
+
 		TabHost tabHost = getTabHost();
 
 		tabHost.addTab(tabHost.newTabSpec("Day").setIndicator("Day")
@@ -49,34 +48,74 @@ public class MainActivity  extends TabActivity implements TabHost.TabContentFact
 				.setContent(this));
 		tabHost.addTab(tabHost.newTabSpec("All").setIndicator("All")
 				.setContent(this));
-		
+
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		stopService(new Intent(MainActivity.this, BackgroundService.class));
 	}
-	
+
 	@Override
 	public View createTabContent(String tag) {
 		// TODO Auto-generated method stub
 		ListView lv = new ListView(this);
+		String myDate = "19.10.10";
+
+		// must read from sql base List of Notifications
 		
-		/*// must read from sql base List of Notifications
-		if (tag.equals("Day"))
-		lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-				new AbstractNotificationGenerator().getListNotification("10.10.10")));
-		else if (tag.equals("Week"))
-				lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-						new AbstractNotificationGenerator().getListNotification("10.10.10")));
-		else if (tag.equals("All"))
-			lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
-					new AbstractNotificationGenerator().getListNotification("10.10.10")));
-		*/
+		ArrayList<Notification> myList = new ArrayList<Notification>();
+		ArrayList<String> myStrList = new ArrayList<String>();
+
+		AbstractNotificationGenerator myANG = new AbstractNotificationGenerator();
+		myList = myANG.getListNotification(myDate);
+		Locale loc = new Locale("ru", "RU");
+		NumberFormat nf = NumberFormat.getNumberInstance(loc);
+		int curDay = 1;
+		try {
+			curDay = nf.parse(myDate.substring(0, 2)).intValue();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (tag.equals("Day")) {
+			for (Notification notification : myList) {
+				if (notification.date.equals(myDate)) {
+					myStrList.add(notification.text);
+				}
+			}
+			lv.setAdapter(new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1, myStrList));
+		} else if (tag.equals("Week")) {
+			for (Notification notification : myList) {
+				if (notification.date.substring(3).equals("10.10")) {
+					int notificationDay = 1;
+					try {
+						notificationDay = nf.parse(
+								notification.date.substring(0, 2)).intValue();
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					if (notificationDay >= curDay - 7) {
+						myStrList.add(notification.text);
+					}
+				}
+			}
+
+			lv.setAdapter(new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1, myStrList));
+		} else if (tag.equals("All")) {
+			for (Notification notification : myList) {
+				myStrList.add(notification.text);
+			}
+			lv.setAdapter(new ArrayAdapter<String>(this,
+					android.R.layout.simple_list_item_1, myStrList));
+		}
 
 		return lv;
 	}
-
 }
