2408951396ca5fbc606cbdebcab21ad190c33859
eugene.bakisov <eugene.bakisov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-18 00:07:17 +0000

    Changed MainActivity class: use new Notification and AbstractNotificationGenerator classes, rewrite some methods

--- notifer/project/src/com/comapping/android/MainActivity.java
+++ notifer/project/src/com/comapping/android/MainActivity.java
@@ -1,12 +1,15 @@
-// Written by Valery Petrov
-// Changed by Fedor Burdun (10.11.2009)
-
+/*
+ * Written by Valery Petrov
+ * Changed by Fedor Burdun (10.11.2009)
+ * Changed by Eugene Bakisov (18.11.2009)   
+ */
 package com.comapping.android;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 
 import android.app.TabActivity;
 import android.os.Bundle;
@@ -19,11 +22,55 @@ import android.widget.TabHost;
 
 public class MainActivity extends TabActivity implements
 		TabHost.TabContentFactory {
+	
 	private static final String TAG = "MainActivity";
-
+	
+	// enumeration of our activity tags:
+	public enum Tag {
+		Day,
+		Week,
+		All;
+		
+		// list of notification's titles for each tag
+		public List<String> titles = new ArrayList<String>();
+	}
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		long day = 1000 * 60 * 60 * 24; // one day in milliseconds
+		Date cur = new Date();
+		Date future = new Date();
+		future.setTime(cur.getTime() + day * 10);
+
+		// must read from sql base List of Notifications
+		// update:   from SQL base via ContentProvider
+		List<Notification> notifications = AbstractNotificationGenerator.
+				generateNotificationList(cur, future, day / 2, 0.5);
+		
+		Calendar calendar = new GregorianCalendar();
+		int curDayNumber = calendar.get(Calendar.DAY_OF_YEAR);
+		int curWeekNumber = calendar.get(Calendar.WEEK_OF_YEAR);
+		
+		// fill tag's title lists:
+		for (Notification n: notifications) {
+			calendar.setTime(n.date);
+			int nDayNumber = calendar.get(Calendar.DAY_OF_YEAR);
+			int nWeekNumber = calendar.get(Calendar.WEEK_OF_YEAR);
+			
+			Tag.All.titles.add(n.title);
+			
+			if (nDayNumber == curDayNumber) {
+				Tag.Day.titles.add(n.title);
+			}
+			
+			if (nWeekNumber == curWeekNumber) {
+				Tag.Week.titles.add(n.title);
+			}
+		}
+		
 		/*
 		 * (setContentView(R.layout.main); Log.d(TAG, "starting service");
 		 * Button bindBtn = (Button) findViewById(R.id.bindBtn);
@@ -41,81 +88,25 @@ public class MainActivity extends TabActivity implements
 		startService(new Intent(MainActivity.this, BackgroundService.class));
 
 		TabHost tabHost = getTabHost();
-
-		tabHost.addTab(tabHost.newTabSpec("Day").setIndicator("Day")
-				.setContent(this));
-		tabHost.addTab(tabHost.newTabSpec("Week").setIndicator("Week")
-				.setContent(this));
-		tabHost.addTab(tabHost.newTabSpec("All").setIndicator("All")
-				.setContent(this));
-
+		
+		for (Tag t: Tag.values()) {
+			tabHost.addTab(tabHost.newTabSpec(t.name()).setIndicator(t.name())
+					.setContent(this));	
+		}
 	}
 
 	@Override
 	protected void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
 		stopService(new Intent(MainActivity.this, BackgroundService.class));
 	}
 
 	@Override
 	public View createTabContent(String tag) {
-		// TODO Auto-generated method stub
 		ListView lv = new ListView(this);
-		String myDate = "19.10.10";
-
-		// must read from sql base List of Notifications
-		
-		ArrayList<Notification> myList = new ArrayList<Notification>();
-		ArrayList<String> myStrList = new ArrayList<String>();
-
-		AbstractNotificationGenerator myANG = new AbstractNotificationGenerator();
-		myList = myANG.getListNotification(myDate);
-		Locale loc = new Locale("ru", "RU");
-		NumberFormat nf = NumberFormat.getNumberInstance(loc);
-		int curDay = 1;
-		try {
-			curDay = nf.parse(myDate.substring(0, 2)).intValue();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		if (tag.equals("Day")) {
-			for (Notification notification : myList) {
-				if (notification.date.equals(myDate)) {
-					myStrList.add(notification.text);
-				}
-			}
-			lv.setAdapter(new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_1, myStrList));
-		} else if (tag.equals("Week")) {
-			for (Notification notification : myList) {
-				if (notification.date.substring(3).equals("10.10")) {
-					int notificationDay = 1;
-					try {
-						notificationDay = nf.parse(
-								notification.date.substring(0, 2)).intValue();
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					if (notificationDay >= curDay - 7) {
-						myStrList.add(notification.text);
-					}
-				}
-			}
-
-			lv.setAdapter(new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_1, myStrList));
-		} else if (tag.equals("All")) {
-			for (Notification notification : myList) {
-				myStrList.add(notification.text);
-			}
-			lv.setAdapter(new ArrayAdapter<String>(this,
-					android.R.layout.simple_list_item_1, myStrList));
-		}
-
+		lv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, 
+				Tag.valueOf(tag).titles));
 		return lv;
 	}
 }
