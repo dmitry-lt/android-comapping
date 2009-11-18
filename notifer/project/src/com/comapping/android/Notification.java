fe6a47c21c6590e4f8715202e226ead137c3f8a8
eugene.bakisov <eugene.bakisov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-18 00:02:01 +0000

    Change Notification class

--- notifer/project/src/com/comapping/android/Notification.java
+++ notifer/project/src/com/comapping/android/Notification.java
@@ -1,27 +1,48 @@
-//Created by Fedor Burdun
-//Example of Notification
-
+/*
+ * Created by Fedor Burdun
+ * Changed by Eugene Bakisov (17.11.2009)
+ */
 package com.comapping.android;
 
 import java.util.Date;
+import java.lang.reflect.Field;
 
+/**
+ *  Contains some information about notification
+ *
+ * @author Fedor Burdun
+ * @author Eugene Bakisov
+ */
 public class Notification {
-	//must be changed
-	public String date; //must be other type
-	public String text;
-	public String link;
-	public String user;
-	public String flag; //must be enum OR NOT?
-	//public Date date;
-	
-	public boolean beWatched;
-	
-	Notification(String date, String text, String link, String user, String flag) {
-		this.date = date;
-		this.text = text;
-		this.link = link;
-		this.user = user;
-		this.flag = flag;
-		this.beWatched = false;
-	}
+    public final String title;
+	public final String link;
+	public final String description;
+
+	public enum Category {Invitation, MapChanges, Tasks, Subscription, Update}
+	public final Category category;
+    
+    public final Date date;
+
+    public Notification(String title, String link, String description, Category category, Date date) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.category = category;
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder("Notification:[");
+        for (Field f: Notification.class.getFields()) {
+            try {
+                sb.append(f.getName() + ": \"" + f.get(this) + "\", ");
+            } catch (IllegalAccessException e) {
+                ;
+            }
+        }
+        sb.replace(sb.length() - 2, sb.length() - 1, "]");
+        return sb.toString();
+    }
 }
+
