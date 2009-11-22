6d1b92577c2645f132680ff81b59d1d755c9d8db
eugene.bakisov <eugene.bakisov@190b583e-fdd6-11dd-9d1a-b59b2e1864b6>
2009-11-22 03:07:47 +0000

    Finally added "provider" folder with NotificationProvider.java and "exceptions" folder.

---
+++ notifer/project/src/com/comapping/android/provider/exceptions/NotImplementedException.java
@@ -0,0 +1,11 @@
+package com.comapping.android.provider.exceptions;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Eugene Bakisov
+ * Date: 22.11.2009
+ * Time: 3:14:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class NotImplementedException extends RuntimeException {
+}
